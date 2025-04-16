53a8fe8b854b402ecb091b33950e7704b596deee
Nithishkumarsp03 <nithishkumarsp03@gmail.com>
2025-04-16 15:19:59 +0530

    Added welcome page

--- src/app.js
+++ src/app.js
@@ -1,14 +1,22 @@
-const express = require('express')
+const express = require("express");
 const app = express();
 require("dotenv").config();
 app.use(express.json());
-const loginRoutes = require('./routes/login/loginRoutes');
-const notesRoutes = require('./routes/notes/notesRoutes');
-const notedetailsRoutes = require('./routes/note-details/notedetails');
+const loginRoutes = require("./routes/login/loginRoutes");
+const notesRoutes = require("./routes/notes/notesRoutes");
+const notedetailsRoutes = require("./routes/note-details/notedetails");
+const path = require("path");
 
 const api = process.env.API;
+app.use(express.static(path.join(__dirname, "public")));
 
-app.get(`${api}/`, (req, res) => {res.send("Hello")})
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
+
+app.get(`${api}/`, (req, res) => {
+  res.send("Hello");
+});
 
 app.use(`${api}/auth`, loginRoutes);
 app.use(`${api}/notes`, notesRoutes);
