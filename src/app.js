26adbfdd5db4f7339a1dd039b0c71cb588a198ac
Nithishkumarsp03 <nithishkumarsp03@gmail.com>
2025-04-17 21:28:58 +0530

    Added cors

--- src/app.js
+++ src/app.js
@@ -6,9 +6,18 @@ const loginRoutes = require("./routes/login/loginRoutes");
 const notesRoutes = require("./routes/notes/notesRoutes");
 const notedetailsRoutes = require("./routes/note-details/notedetails");
 const path = require("path");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
 const api = process.env.API;
 app.use(express.static(path.join(__dirname, "public")));
+app.use(cors({
+  origin: '*', // Or specify your allowed origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+app.options('*', cors());
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
